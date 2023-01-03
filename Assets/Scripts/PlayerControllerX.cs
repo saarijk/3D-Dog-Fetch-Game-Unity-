ef31d820c8e34a6e8e996a94e7fa7ce32e956af3
jsaari <jsaari@student.oulu.fi>
2023-01-03 13:53:57 +0200

    Initial Project

---
+++ Assets/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogDelay = 1f;
+    private bool _dogReady = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && _dogReady)
+        {
+            _dogReady = false;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Invoke("getDogReady", dogDelay);
+        }
+    }
+
+    private void getDogReady()
+    {
+        _dogReady = true;
+    }
+}
